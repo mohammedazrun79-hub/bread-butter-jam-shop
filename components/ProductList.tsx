13c2232eb574efc2dce26636a75bca2d40262bd2
mohammedazrun79-hub <mohammedazrun79@gmail.com>
2026-05-25 09:30:45 +0530

    Add ProductList component

---
+++ components/ProductList.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { products } from '@/lib/products';
+import ProductCard from './ProductCard';
+
+export default function ProductList() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
